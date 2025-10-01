94224d625592678e36fefccde2f6408270e40b20
song zhaohui <a554381721@gmail.com>
2025-10-01 10:51:22 +0000

    add convergence study of parallel diffusion reaction base

---
+++ lab-05/src/Poisson3D_parallel.hpp
@@ -0,0 +1,241 @@
+#ifndef POISSON_3D_HPP
+#define POISSON_3D_HPP
+
+#include <deal.II/base/conditional_ostream.h>
+#include <deal.II/base/quadrature_lib.h>
+
+#include <deal.II/distributed/fully_distributed_tria.h>
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_simplex_p.h>
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/mapping_fe.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_in.h>
+#include <deal.II/grid/grid_out.h>
+#include <deal.II/grid/grid_tools.h>
+#include <deal.II/grid/tria.h>
+
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/precondition.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/solver_gmres.h>
+#include <deal.II/lac/trilinos_precondition.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
+#include <deal.II/lac/vector.h>
+
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/numerics/vector_tools.h>
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+using namespace dealii;
+
+/**
+ * Class managing the differential problem.
+ */
+class Poisson3DParallel
+{
+public:
+  // Physical dimension (1D, 2D, 3D)
+  static constexpr unsigned int dim = 2;
+
+  // Diffusion coefficient.
+  // In deal.ii, functions are implemented by deriving the dealii::Function
+  // class, which provides an interface for the computation of function values
+  // and their derivatives.
+  class DiffusionCoefficient : public Function<dim>
+  {
+  public:
+    // Constructor.
+    DiffusionCoefficient()
+    {}
+
+    // Evaluation.
+    virtual double
+    value(const Point<dim> & /*p*/,
+          const unsigned int /*component*/ = 0) const override
+    {
+      return 1.0;
+    }
+  };
+
+  // Reaction coefficient.
+  class ReactionCoefficient : public Function<dim>
+  {
+  public:
+    // Constructor.
+    ReactionCoefficient()
+    {}
+
+    // Evaluation.
+    virtual double
+    value(const Point<dim> & /*p*/,
+          const unsigned int /*component*/ = 0) const override
+    {
+      return 1.0;
+    }
+  };
+
+  // Forcing term.
+  class ForcingTerm : public Function<dim>
+  {
+  public:
+    // Constructor.
+    ForcingTerm()
+    {}
+
+    // Evaluation.
+    virtual double
+    value(const Point<dim> & p,
+          const unsigned int /*component*/ = 0) const override
+    {
+      return (20.0 * M_PI * M_PI + 1.0) * sin(2.0 * M_PI * p[0]) * sin(4.0 * M_PI * p[1]);
+    }
+  };
+
+   // Dirichlet boundary conditions.
+  class FunctionG : public Function<dim>
+  {
+  public:
+    // Constructor.
+    FunctionG()
+    {}
+
+    // Evaluation.
+    virtual double
+    value(const Point<dim> & /*p*/,
+          const unsigned int /*component*/ = 0) const override
+    {
+      return 0;
+    }
+  };
+
+  // Exact solution.
+  class ExactSolution : public Function<dim>
+  {
+  public:
+    // Constructor.
+    ExactSolution()
+    {}
+
+    // Evaluation.
+    virtual double
+    value(const Point<dim> &p,
+          const unsigned int /*component*/ = 0) const override
+    {
+      return sin(2.0 * M_PI * p[0]) * sin(4.0 * M_PI * p[1]);
+    }
+
+    // Gradient evaluation.
+    // deal.II requires this method to return a Tensor (not a double), i.e. a
+    // dim-dimensional vector. In our case, dim = 1, so that the Tensor will in
+    // practice contain a single number. Nonetheless, we need to return an
+    // object of type Tensor.
+    virtual Tensor<1, dim>
+    gradient(const Point<dim> &p,
+             const unsigned int /*component*/ = 0) const override
+    {
+      Tensor<1, dim> result;
+
+      // Points 3 and 4.
+      result[0] = 2 * M_PI * cos(2 * M_PI * p[0]) * sin(4 * M_PI * p[1]);
+
+      result[1] =  4 * M_PI * sin(2 * M_PI * p[0]) * cos(4 * M_PI * p[1]);
+
+      return result;
+    }
+  };
+
+
+  // Constructor.
+  Poisson3DParallel(const std::string &mesh_file_name_, const unsigned int &r_)
+    : mesh_file_name(mesh_file_name_)
+    , r(r_)
+    , mpi_size(Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD))
+    , mpi_rank(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD))
+    , mesh(MPI_COMM_WORLD)
+    , pcout(std::cout, mpi_rank == 0)
+  {}
+
+  // Initialization.
+  void
+  setup();
+
+  // System assembly.
+  void
+  assemble();
+
+  // System solution.
+  void
+  solve();
+
+  // Output.
+  void
+  output() const;
+
+  double compute_error(const VectorTools::NormType &norm_type) const;
+
+
+protected:
+  // Path to the mesh file.
+  const std::string mesh_file_name;
+
+  // Polynomial degree.
+  const unsigned int r;
+
+  // Number of MPI processes.
+  const unsigned int mpi_size;
+
+  // This MPI process.
+  const unsigned int mpi_rank;
+
+  // Diffusion coefficient.
+  DiffusionCoefficient diffusion_coefficient;
+
+  // Reaction coefficient.
+  ReactionCoefficient reaction_coefficient;
+
+  // Forcing term.
+  ForcingTerm forcing_term;
+
+  // g(x).
+  FunctionG function_g;
+
+  // Triangulation. The parallel::fullydistributed::Triangulation class manages
+  // a triangulation that is completely distributed (i.e. each process only
+  // knows about the elements it owns and its ghost elements).
+  parallel::fullydistributed::Triangulation<dim> mesh;
+
+  // Finite element space.
+  std::unique_ptr<FiniteElement<dim>> fe;
+
+  // Quadrature formula.
+  std::unique_ptr<Quadrature<dim>> quadrature;
+
+  // DoF handler.
+  DoFHandler<dim> dof_handler;
+
+  // System matrix.
+  TrilinosWrappers::SparseMatrix system_matrix;
+
+  // System right-hand side.
+  TrilinosWrappers::MPI::Vector system_rhs;
+
+  // System solution.
+  TrilinosWrappers::MPI::Vector solution;
+
+  // Parallel output stream.
+  ConditionalOStream pcout;
+
+  // DoFs owned by current process.
+  IndexSet locally_owned_dofs;
+};
+
+#endif
